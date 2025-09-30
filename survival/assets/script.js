2ac4f95739437fd4ddb58810beaee0471bf4a398
Vinicius Vieira <vinicius3cta@gmail.com>
2025-09-30 14:50:58 -0300

    Delete survival/assets directory

--- survival/assets/script.js
+++
@@ -1,9 +0,0 @@
-<script id="__script_js__">
-function filterTopics(){
-const q=(document.querySelector('#q')?.value||'').toLowerCase();
-document.querySelectorAll('[data-topic]').forEach(el=>{
-const txt=el.getAttribute('data-topic');
-el.style.display = (!q || txt.includes(q))? 'block':'none';
-});
-}
-</script>
